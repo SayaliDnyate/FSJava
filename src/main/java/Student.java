d5e23e32ab54a5b7761cc688de4b79c6be9f850c
SayaliDnyate <sayalidnyate@gmail.com>
2024-04-01 21:17:30 +0530

    changes

---
+++ src/main/java/Student.java
@@ -0,0 +1,16 @@
+// Java Program for class example
+
+class Student {
+    int id;
+    String name;
+
+    public static void main(String args[]) {
+        Student s1 = new Student();
+        System.out.println(s1.id);
+        System.out.println(s1.name);
+    }
+        public static void play (String args[])
+        {
+            System.out.println("I am playing");
+        }
+}
