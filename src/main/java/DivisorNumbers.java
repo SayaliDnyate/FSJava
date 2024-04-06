2a357d8f4e9490e5456c756275dc141f26634779
SayaliDnyate <sayalidnyate@gmail.com>
2024-04-06 16:20:03 +0530

    updated

--- src/main/java/DivisorNumbers.java
+++ src/main/java/DivisorNumbers.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class DivisorNumbers {
+/* Program to print divisors of given number */
+// input number = 36
+// output numbers =  1 2 3 4 6 9 12 18 36
+class DivisionNumbers{
+    public static void main(String [] args){
+        int n = 36;
+        System.out.println("Factors of "+n+" are: ");
+
+        // Loop runs from 1 to 36
+        for(int i=1; i<=n; ++i){
+
+        // If i is divisible by 1 then i is the factor of provided number 36
+        if ( n % i==0 ){
+
+            System.out.println(i + " ");
+        }
+
+        }
+
+    }
 }
--- src/main/java/ReverseNumber.java
+++ src/main/java/ReverseNumber.java
@@ -1,2 +1,49 @@
-package PACKAGE_NAME;public class ReverseNumber {
+/* reverse number program */
+// input number = 123
+// output number = 321
+
+class ReverseNumber {
+    public static void main(String[] args)
+    {
+        int num = 123, reversed = 0;
+        System.out.println("Original Number: "+num);
+
+     //Run loop until num becomes 0
+        while(num !=0) {
+
+     // Get last digit from num
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+
+    // Remove the last digit from num
+            num = num/10;
+        }
+        System.out.println("Reversed number: " + reversed);
+    }
 }
+
+
+//Example 2:
+// input number = 234
+// output number = 432
+class ReverseNumbers {
+    public static void main(String[] args)
+    {
+        int num = 234, reversed = 0;
+        System.out.println("Original Number: "+num);
+
+        //Run loop until num becomes 0
+        while(num !=0) {
+
+            // Get last digit from num
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+
+            // Remove the last digit from num
+            num = num/10;
+        }
+        System.out.println("Reversed number: " + reversed);
+    }
+}
+
+
